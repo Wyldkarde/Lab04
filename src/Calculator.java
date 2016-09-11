d263802f08e02e35f89eb690fb4bc13c5708b1e0
Wyldkarde <Callendross@yahoo.com>
2016-09-11 16:52:40 -0400

    Sun morning edits

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,46 +1,88 @@
+package solution;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 
 
+
+
+/**
+ * @author Dillingham, Jace
+ * @version 09/10/2016
+ *
+ */
 
 public class Calculator
 {
     private JFrame calcFrame;
     private JTextField leftOperand;
     private JTextField rightOperand;
-    private jlResults = new JLabel("Results = " + sum);
-    public JFrame getcalcFrame()
-    private int sum = 0;
     
-    {
-        return calcFrame;
-    }
-
+    /**
+     * Calculator constructor.
+     */
     public Calculator()
     {
         JFrame   calculatorFrame;
         calculatorFrame = new JFrame();
-        calculatorFrame.setLocation(100,100);
-        calculatorFrame.setSize(400,400);
+        calculatorFrame.setLocation(100, 100);
+        calculatorFrame.setSize(400, 400);
         calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calculatorFrame.setTitle("Basic Calculator");
 
-        initializeComponents();  //Create and add all components
+        setFrame(calculatorFrame);
+        initializeComponents(); 
+
         calculatorFrame.setVisible(true);
-    }
-    public void initializeComponents()
-    {
-        Calculator.add(calcNorth(), BorderLayout.PAGE_START);
-        Calculator.add(calcCenter(), BorderLayout.CENTER);
-        Calculator.add(results(),BorderLayout.PAGE_END);
+        
     }
 
+    /**
+     * Main method.
+     * @param args unused.
+     */
+    public static void main(String args[])
+    {
+        Calculator c = new Calculator();
+    }
+ 
+    /**
+     *  Accessor for calcFrame.
+     * @return calcFrame.
+     */
+    public JFrame getFrame()
+    {
+        return calcFrame;
+    }    
+    
+    /**
+     * Mutator for calcFrame.
+     * @param calcFrame
+     */
+    public void setFrame(JFrame calcFrame)
+    {
+        this.calcFrame = calcFrame;
+    }
+    
+    /**
+     * Constructor for components.
+     */
+    public void initializeComponents()
+    {   
+        JFrame frm = this.getFrame();
+        frm.add(calcNorth(), BorderLayout.PAGE_START);
+        frm.add(results(), BorderLayout.CENTER);
+        frm.add(calcCenter(), BorderLayout.PAGE_END);
+    }
+    
+    /**
+     * Constructor for north calc panel.
+     * @return north panel of calculator.
+     */
     private JPanel calcNorth()
     {
         JTextField leftOperand = new JTextField(10);
@@ -49,39 +91,58 @@ public class Calculator
         JPanel calcNorth = new JPanel();
         calcNorth.add(leftOperand);
         calcNorth.add(rightOperand);
+        //added return
+        return calcNorth;
     }
+    
+    /**
+     * Constructor for center calc panel.
+     * @return center calc panel.
+     */
     private JPanel calcCenter()
     {
-        JPanel calcCenter = new JPanel();
-        calcCenter.add(jlResults);
-    }
-    private JPanel calcSouth()
-    {
-        JButton addButton = new JButton("ADD");
-        JButton subButton = new JButton("SUB");
-        JButton multButton = new JButton("MULT");
-        JButton divButton = new JButton("DIV");
-                
+        JButton addButton = new JButton("+");
+        JButton subButton = new JButton("-");
+        JButton multButton = new JButton("*");
+        JButton divButton = new JButton("/");
+        JButton calcTotal = new JButton("=");
+        
         JPanel calcCenter = new JPanel();
         calcCenter.add(addButton);
         calcCenter.add(subButton);
         calcCenter.add(multButton);
         calcCenter.add(divButton);
-        
+        calcCenter.add(calcTotal);
+
+        return calcCenter;
     }
     
+    /**
+     * Constructor for result panel.
+     * @return results panel.
+     */
+    private JPanel results()
+    {
+        JPanel resPanel = new JPanel();
+        JLabel resLabel = new JLabel();
+        
+        return resPanel;
+    }
+    
+    /**
+     * Updater for button click.
+     */
     public void updateClicked()
     {
         //Get the user text from the text field
-        String t = leftOperand.getText();
+        String results = leftOperand.getText();
 
         //Check to see if it is blank
-        if (!t.trim().equals(""))
+        if (!results.trim().equals(""))
         {
             //If it is not blank set the test on the label
-            rightOperand.setText(t);
+            rightOperand.setText(results);
         }
         
     }
-
 }
